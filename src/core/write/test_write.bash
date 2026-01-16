76198f269971ed85d764f3118ef2b697d68a86a7
rzaldana <raularmandozc12@gmail.com>
2026-01-16 16:39:09 -0500

    add Makefile to write module

---
+++ src/core/write/test_write.bash
@@ -0,0 +1,18 @@
+# Get the directory of the script that's currently running
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+LIBRARY_PATH="$SCRIPT_DIR/dist/write.bash"
+
+test_write_sends_message_to_configured_fd() {
+  set -euo pipefail
+  source "$LIBRARY_PATH"
+  local message="my message"
+  # Create tmpfile to store output
+  tmpfile="$(mktemp)"
+  # shellcheck disable=SC2064
+  trap "rm $tmpfile" EXIT
+
+  __blog.write.set_destination_fd "3"
+  __blog.write.write <<<"$message" 3>"$tmpfile"
+
+  assert_no_diff "$tmpfile" <(echo "$message")
+}
