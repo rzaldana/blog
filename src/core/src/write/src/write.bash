83604d76259925d7f21f070787c93ac4b049bf85
rzaldana <raularmandozc12@gmail.com>
2026-01-17 10:07:14 -0500

    rename write to core.write

--- src/core/src/write/src/write.bash
+++ src/core/src/write/src/write.bash
@@ -1,9 +1,9 @@
 
-__log.write.get_default_destination_fd() {
+__log.core.write.get_default_destination_fd() {
   echo "2"
 }
 
-__log.write.is_destination_fd_set() {
+__log.core.write.is_destination_fd_set() {
   if [[ -n "${__BLOG_WRITE_DESTINATION_FD:-}" ]]; then
     return 0
   else
@@ -11,12 +11,12 @@ __log.write.is_destination_fd_set() {
   fi
 }
 
-__log.write.set_destination_fd() {
+__log.core.write.set_destination_fd() {
   __BLOG_WRITE_DESTINATION_FD="$1"
   export __BLOG_WRITE_DESTINATION_FD
 }
 
-__log.write.write() {
+__log.core.write.write() {
   local destination_fd
   destination_fd="${__BLOG_WRITE_DESTINATION_FD}"
   while IFS= read -r log_line; do
